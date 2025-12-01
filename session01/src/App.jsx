4f2a7664aff3f84783fbf0274515ad35c568e7c8
jamigit <ingmicheljasmin@hotmail.fr>
2025-12-01 15:37:45 -0500

    My Second Commit

--- session01/src/App.jsx
+++ session01/src/App.jsx
@@ -3,11 +3,19 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+function Header() {
+  return (
+    <h1>Welcome to MBDS </h1>
+  );
+}
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Header/>
+
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
