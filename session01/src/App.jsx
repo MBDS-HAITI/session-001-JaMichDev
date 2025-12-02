1589bc70beb573023b3e13e1f9848d079a6c34e2
jamigit <ingmicheljasmin@hotmail.fr>
2025-12-01 21:15:02 -0500

    Modification

--- session01/src/App.jsx
+++ session01/src/App.jsx
@@ -3,19 +3,11 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function Header() {
-  return (
-    <h1>Welcome to MBDS TD</h1>
-  );
-}
-
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Header/>
-
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -24,19 +16,17 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>My first TD React</h1>
+      <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
-           Je peux modifier le code
         </p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-        MBDS is cool for me Jasmin !
       </p>
     </>
   )
