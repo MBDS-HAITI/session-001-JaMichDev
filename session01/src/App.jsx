d13bacca55735bf0cbcd314a05ad1256ff9a04e7
jamigit <ingmicheljasmin@hotmail.fr>
2025-12-01 21:53:05 -0500

    TD01 : Ajouter un composant Header dans la page

--- session01/src/App.jsx
+++ session01/src/App.jsx
@@ -1,7 +1,25 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import mbdsLogo from '/mbds_logo_transparent.svg'
 import './App.css'
+
+function Header() {
+  return (
+   <>
+       <div>
+            <a href="https://fds.edu.ht/inscriptionMBDS/user/login" target="_blank">
+                <img src={mbdsLogo} className="logo" alt="Vite logo" />
+            </a>
+            <a href="https://react.dev" target="_blank">
+               <img src={reactLogo} className="logo react" alt="React logo" />
+            </a>
+
+            <h1>Introduction à React</h1>
+            <h2>À la découverte des premières notions de React</h2>
+       </div>
+   </>
+  );
+}
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,27 +27,20 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+           <Header />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      
+
+
     </>
   )
 }
+
+const todo = {
+  title: "Todo 1",
+  description:"Description 1",
+  status: "completed"
+};
 
 export default App
